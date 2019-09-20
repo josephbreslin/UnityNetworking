d1336146690edac650e8094196bd8313313fd30c
josephbreslin <c15713979@mydit.ie>
2019-09-21 00:40:36 +0100

    Unity Client

---
+++ Client/Assets/scripts/LightGrid.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class LightGrid : MonoBehaviour
+{
+    public float cubeSize = .5f;
+    List<GameObject> cubes = new List<GameObject>();
+
+    //Client
+    int port = 1510;
+
+    void Start()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 10; j++)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.localScale= new Vector3(cubeSize, cubeSize, cubeSize);
+                cube.transform.position = new Vector3(i, j, 0);
+                cubes.Add(cube);
+            }
+        }
+        Camera.main.transform.position = new Vector3(5, 5, -8);
+        SetCubeColors(45);
+    }
+
+    void SetCubeColors(int index)
+    {
+        for(int i = 0; i < cubes.Count; i++)
+        {
+            if(i == index)
+            {
+                cubes[i].GetComponent<Renderer>().material.color = Color.magenta;
+            }
+            else
+            {
+                cubes[i].GetComponent<Renderer>().material.color = Color.white;
+            }
+        }
+    }
+    public void RequestNumber()
+    {
+        Debug.Log("Requesting number from server");
+        ActivateClient();
+    }
+
+    public void ActivateClient()
+    {
+        try
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
+
+            Socket sender = new Socket(ipAddr.AddressFamily,
+                                        SocketType.Stream,
+                                        ProtocolType.Tcp);
+
+            try
+            {
+                sender.Connect(localEndPoint);
+                Debug.Log("Socket Connected to " +sender.RemoteEndPoint.ToString());
+
+                byte[] messageSent = Encoding.ASCII.GetBytes("Test Client <EOF>");
+                int byteSent = sender.Send(messageSent);
+
+                byte[] messageReceived = new byte[1024];
+                int byteRecv = sender.Receive(messageReceived);
+                Debug.Log("Message from Server -> " + Encoding.ASCII.GetString(messageReceived, 0, byteRecv));
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Debug.Log("ArgumentNullException " + ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Debug.Log("SocketException " + se.ToString());
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Unexpected exception " + e.ToString());
+            }
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;   
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    static public class Program
+    {
+        static int maxPlayerCount;
+        static int port = 1510;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Maximum number of players that can sign into this server, Between 2 and 20");
+            string str = Console.ReadLine();
+            maxPlayerCount = Convert.ToInt32(str);
+
+            if(maxPlayerCount >20)
+                maxPlayerCount = 20;
+            if(maxPlayerCount < 2)
+                maxPlayerCount = 2;
+            
+            ActivateServer();
+        }
+
+        static void ActivateServer()
+        {
+            Console.WriteLine("Server activated! \nClient capacity is set to {0}", maxPlayerCount);
+
+            //Get host address
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
+
+            //Open Socket
+            Socket listener = new Socket(ipAddr.AddressFamily,SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(maxPlayerCount);
+
+                while(true)
+                {
+                    Console.WriteLine("Waiting for connection");
+                    Socket clientSocket = listener.Accept();
+
+                    string data = null;
+                    Byte[] bytes = new Byte[1024];
+
+                    while(true)
+                    {
+                        int numBytes = clientSocket.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, numBytes);
+
+                        if(data.IndexOf("<EOF>") > -1)
+                            break;
+                    }
+                    Console.WriteLine("Text Recieved --> {0}", data);
+                    Random random = new Random();
+                    int gridOutput = random.Next(0, 100);
+                    byte[] message = Encoding.ASCII.GetBytes(gridOutput.ToString());
+                    clientSocket.Send(message);
+
+                    //Experiment without this feature, or with this feature on command from the client
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                //Log errors
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
